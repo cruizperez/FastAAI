05a4c75c5d94ee594d71f6c6486afa09001e4aa1
cruizperez <cruiz_perez@hotmail.com>
2020-12-14 14:10:43 -0500

    Removed cosine

--- FastAAI/FastAAI.py
+++ FastAAI/FastAAI.py
@@ -24,8 +24,6 @@ from pathlib import Path
 from sys import argv
 from sys import exit
 from functools import partial
-from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.feature_extraction.text import CountVectorizer
 
 
 ################################################################################
